b25dcd898c0325e93da98b2c775182dc35566c2a
Mathias Olsson <hypp74@gmail.com>
2019-04-27 14:49:21 +0200

    fixed animation

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,24 +10,17 @@ game.PlayerEntity = me.Sprite.extend({
         // call the constructor
         this._super(me.Sprite, 'init', [x, y , settings]);
 
-        // max walking & jumping speed
-        //this.body.setMaxVelocity(3, 15);
-        //this.body.setFriction(0.4, 0);
-
         // set the display to follow our position on both axis
         me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH, 0.4);
 
-        // ensure the player is updated even when outside of the viewport
-        this.alwaysUpdate = true;
-
         // define a basic walking animation (using all frames)
-        //this.renderable.addAnimation("walk",  [0, 1, 2, 3, 4, 5, 6, 7]);
+        this.addAnimation("walk",  [0, 1, 2, 3]);
 
         // define a standing animation (using the first frame)
-        //this.renderable.addAnimation("stand",  [0]);
+        this.addAnimation("stand",  [0]);
 
         // set the standing animation as default
-        //this.renderable.setCurrentAnimation("stand");
+        this.setCurrentAnimation("stand");
     },
 
     /**
@@ -56,5 +49,36 @@ game.ComputerEntity = me.Sprite.extend({
     init : function (x, y, settings) {
         // call the constructor
         this._super(me.Sprite, 'init', [x, y , settings]);
+
+        // define a basic walking animation (using all frames)
+        this.addAnimation("walk",  [4, 5, 6, 7]);
+
+        // define a standing animation (using the first frame)
+        this.addAnimation("stand",  [4]);
+
+        // set the standing animation as default
+        this.setCurrentAnimation("stand");
+    },
+
+    /**
+     * update the entity
+     */
+    update : function (dt) {
+
+        // handle collisions against other shapes
+        me.collision.check(this);
+
+        // return true if we moved or if the renderable was updated
+        return this._super(me.Sprite, 'update', [dt]);
+    },
+
+   /**
+     * colision handler
+     * (called when colliding with other objects)
+     */
+    onCollision : function (response, other) {
+        // Make all other objects solid
+        return true;
     }
+
   });
