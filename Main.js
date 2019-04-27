2a3652b14fddefa52138134d41c17bb61d538868
Mathias Olsson <hypp74@gmail.com>
2019-04-27 20:26:55 +0200

    cleanup

--- Main.js
+++ Main.js
@@ -1,14 +1,32 @@
 
+
+
+function tilesFromImage(img, width, height) {
+    let tiles = [];
+
+    //Create the `tileset` sprite from the texture
+    let baseTexture = PIXI.BaseTexture.fromImage(img);
+
+    for (let y = 0; y < baseTexture.height; y += height) {
+        for (let x = 0; x < baseTexture.width; x += width) {
+            let rectangle = new PIXI.Rectangle(x, y, width, height);
+            let texture = new PIXI.Texture(baseTexture, rectangle);
+    
+            tiles.push(texture);
+        }
+    }
+
+    return tiles;
+}
+
+
 function Main() {
 
     //Aliases
-    let Application = PIXI.Application,
-        loader = PIXI.loader,
-        resources = PIXI.loader.resources,
-        Sprite = PIXI.Sprite;
+    let resources = PIXI.loader.resources;
 
     //Create a Pixi Application
-    let app = new Application({ 
+    let app = new PIXI.Application({ 
         width: 640, 
         height: 480,                       
         antialias: true, 
@@ -24,104 +42,10 @@ function Main() {
     document.body.appendChild(app.view);
 
     //load an image and run the `setup` function when it's done
-    loader
-    .add("resources/roadtiles.png")
-    .load(setup);
-
-    //This `setup` function will run when the image has loaded
-    function setup() {
-
-        //Create the cat sprite
-        let cat = new Sprite(resources["resources/roadtiles.png"].texture);
-        cat.x = 196;
-        cat.y = 196;
-        cat.rotation = 0.5;
-        cat.anchor.x = 0.5;
-        cat.anchor.y = 0.5;
-        
-        //Add the cat to the stage
-        app.stage.addChild(cat);
-
-
-        // 20x15
-        level = [
-            -1,-1,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1,
-            -1,-1,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1,
-            -1,-1,-1,-1,-1, -1, 2, 1, 1, 1,  1, 3,-1,-1,-1,  2, 1, 3,-1,-1,
-            -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1, 0,-1,-1,-1,  0,-1, 0,-1,-1,
-            -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1, 4, 1, 1, 1,  5,-1, 0,-1,-1,
-
-            -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1, 0,-1,-1,
-            -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1, 0,-1,-1,
-            -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1, 0,-1,-1,
-            -1,-1,-1,-1,-1, -1, 4, 1, 3,-1,  2, 1, 1, 1, 1,  1, 1, 5,-1,-1,
-            -1, 2, 1, 1, 1,  3,-1,-1, 0,-1,  0,-1,-1,-1,-1, -1,-1,-1,-1,-1,
-
-            -1, 0,-1,-1,-1,  4, 1, 1, 5,-1,  4, 1, 1, 1, 1,  3,-1,-1,-1,-1,
-            -1, 0,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1,  0,-1,-1,-1,-1,
-            -1, 4, 1, 1, 1,  1, 1, 1, 1, 3, -1, 2, 1, 1, 1,  5,-1,-1,-1,-1,
-            -1,-1,-1,-1,-1, -1,-1,-1,-1, 0, -1, 0,-1,-1,-1, -1,-1,-1,-1,-1,
-            -1,-1,-1,-1,-1, -1,-1,-1,-1, 4,  1, 5,-1,-1,-1, -1,-1,-1,-1,-1,
-        ]
-
-        roadList = [];
-
-        //Create the `tileset` sprite from the texture
-        let baseTexture = PIXI.BaseTexture.fromImage("resources/roadtiles.png");
-
-        for (let y = 0; y < baseTexture.height; y += 32) {
-            for (let x = 0; x < baseTexture.width; x += 32) {
-                let rectangle = new PIXI.Rectangle(x, y, 32, 32);
-                let roadTexture = new PIXI.Texture(baseTexture, rectangle);
-        
-                roadList.push(roadTexture);
-            }
-        }
-
-        for (let i = 0; i < 15; i++) {
-            let y = i * 32;
-            for (let j = 0; j < 20; j++) {
-                let x = j*32;
-                let idx = i*20+j;
-                let tile = level[idx];
-                if (tile >= 0) {
-                    let roadTexture = roadList[tile];
-
-                    //Create the sprite from the texture
-                    let road = new Sprite(roadTexture);
-            
-                    road.x = x;
-                    road.y = y;
-            
-                    app.stage.addChild(road)
-                }
-            }    
-        }
-
-    }
-
-    this.scroller = new Scroller(this.stage);
-
-    /*
-    this.stage = new PIXI.Container();
-    this.renderer = PIXI.autoDetectRenderer(
-        512,
-        384,
-        {view:document.getElementById("game-canvas")}
-    );
-
-    this.scroller = new Scroller(this.stage);
-
     PIXI.loader
     .add("resources/roadtiles.png")
-    .load(function (stage) {
-        cat = new PIXI.Sprite(PIXI.loader.resources["resources/roadtiles.png"].texture);
-        stage.addChild(cat);    
-    }(this.stage));
-
-    */
-
-    requestAnimationFrame(this.update.bind(this));
+    .add("resources/cars.png")
+    .load(this.setup.bind(this));
 
 }
 
@@ -130,6 +54,69 @@ Main.SCROLL_SPEED = 5;
 Main.prototype.update = function() {
     this.scroller.moveViewportXBy(Main.SCROLL_SPEED);
     this.app.renderer.render(this.stage);
-    requestAnimationFrame(this.update.bind(this));
 };
 
+Main.prototype.gameLoop = function(delta) {
+    this.update();
+}
+
+Main.prototype.setup = function() {
+    // 20x15
+    level = [
+        -1,-1,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1, -1, 2, 1, 1, 1,  1, 3,-1,-1,-1,  2, 1, 3,-1,-1,
+        -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1, 0,-1,-1,-1,  0,-1, 0,-1,-1,
+        -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1, 4, 1, 1, 1,  5,-1, 0,-1,-1,
+
+        -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1, 0,-1,-1,
+        -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1, 0,-1,-1,
+        -1,-1,-1,-1,-1, -1, 0,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1, 0,-1,-1,
+        -1,-1,-1,-1,-1, -1, 4, 1, 3,-1,  2, 1, 1, 1, 1,  1, 1, 5,-1,-1,
+        -1, 2, 1, 1, 1,  3,-1,-1, 0,-1,  0,-1,-1,-1,-1, -1,-1,-1,-1,-1,
+
+        -1, 0,-1,-1,-1,  4, 1, 1, 5,-1,  4, 1, 1, 1, 1,  3,-1,-1,-1,-1,
+        -1, 0,-1,-1,-1, -1,-1,-1,-1,-1, -1,-1,-1,-1,-1,  0,-1,-1,-1,-1,
+        -1, 4, 1, 1, 1,  1, 1, 1, 1, 3, -1, 2, 1, 1, 1,  5,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1, -1,-1,-1,-1, 0, -1, 0,-1,-1,-1, -1,-1,-1,-1,-1,
+        -1,-1,-1,-1,-1, -1,-1,-1,-1, 4,  1, 5,-1,-1,-1, -1,-1,-1,-1,-1,
+    ]
+
+    roadList = tilesFromImage("resources/roadtiles.png", 32, 32);
+
+    for (let i = 0; i < 15; i++) {
+        let y = i * 32;
+        for (let j = 0; j < 20; j++) {
+            let x = j*32;
+            let idx = i*20+j;
+            let tile = level[idx];
+            if (tile >= 0) {
+                let roadTexture = roadList[tile];
+
+                //Create the sprite from the texture
+                let road = new PIXI.Sprite(roadTexture);
+        
+                road.x = x;
+                road.y = y;
+        
+                this.app.stage.addChild(road)
+            }
+        }    
+    }
+
+
+    carList = tilesFromImage("resources/cars.png", 16, 32);
+    let carTexture = carList[0];
+
+    //Create the sprite from the texture
+    let car = new PIXI.Sprite(carTexture);
+
+    car.x = 32*6+(32-16)/2;
+    car.y = 32*5;
+
+    this.app.stage.addChild(car)
+
+    this.scroller = new Scroller(this.stage);
+
+    this.app.ticker.add(this.gameLoop.bind(this));
+}
