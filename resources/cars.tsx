f437756b81c051555c81d708be540f1577a2b9e4
Mathias Olsson <hypp74@gmail.com>
2019-04-28 23:46:59 +0200

    cleanup before release

--- resources/cars.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="cars" tilewidth="16" tileheight="32" tilecount="16" columns="4">
- <image source="../data/img/sprite/cars.png" width="64" height="128"/>
-</tileset>
